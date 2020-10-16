57cb1b45f254813829599a20ef5f080256c70654
Jay Berkenbilt <ejb@ql.org>
2020-10-16 05:18:09 -0400

    -

---
+++ build-windows.bat
@@ -0,0 +1,12 @@
+@echo on
+@rem Usage: build-windows {32|64} {msvc|mingw}
+if %2 == msvc (
+    if %1 == 64 (
+       call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+    ) else (
+       call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars32.bat"
+    )
+    bash ./build-windows %1 %2
+) else (
+    C:\msys64\usr\bin\env.exe MSYSTEM=MINGW64 /bin/bash -l %CD%/build-windows %1 %2
+)
