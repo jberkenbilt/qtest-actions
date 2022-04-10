26cfb1ae5008eba126e2769e9693ecd2e8485d2c
Jay Berkenbilt <ejb@ql.org>
2022-04-10 13:50:02 -0400

    Try with visibility

--- apple-clang/a.cc
+++ apple-clang/a.cc
@@ -1,17 +1,32 @@
 #include <iostream>
 
 #if defined _WIN32 || defined __CYGWIN__
-# define QPDF_DLL__VISIBLE "v1"
-# define QPDF_DLL__HIDDEN "v2"
+# define QPDF_DLL__VISIBLE __declspec(dllexport)
+# define QPDF_DLL__HIDDEN
+# define BRANCH1 "1"
 #elif defined __GNUC__
-# define QPDF_DLL__VISIBLE "v3"
-# define QPDF_DLL__HIDDEN "v4"
+# define QPDF_DLL__VISIBLE __attribute__((visibility("default")))
+# define QPDF_DLL__HIDDEN __attribute__((visibility("hidden")))
+# define BRANCH1 "2"
 #else
-# define QPDF_DLL__VISIBLE "v5"
-# define QPDF_DLL__HIDDEN "v6"
+# define QPDF_DLL__VISIBLE
+# define QPDF_DLL__HIDDEN
+# define BRANCH1 "3"
 #endif
 
+#define QPDF_DLL QPDF_DLL__VISIBLE
+#if defined _WIN32 || defined __CYGWIN__
+# define QPDF_DLL_PRIVATE
+# define QPDF_DLL_CLASS
+# define BRANCH2 "4"
+#else
+# define QPDF_DLL_PRIVATE QPDF_DLL__HIDDEN
+# define QPDF_DLL_CLASS QPDF_DLL__VISIBLE
+# define BRANCH2 "5"
+#endif
+
+QPDF_DLL
 void f()
 {
-    std::cout << QPDF_DLL__VISIBLE << " " << QPDF_DLL__HIDDEN << std::endl;
+    std::cout << BRANCH1 << " " << BRANCH2 << std::endl;
 }
